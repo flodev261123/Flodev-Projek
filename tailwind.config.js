f3b03e676678cfb9051501364257dcece83b1c23
alip <haikalsialip999@gmail.com>
2024-07-27 13:03:21 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      animation: {
+        // adjust speed according to your need
+        marquee: "marquee 40s linear infinite",
+      },
+      transitionTimingFunction: {
+        "in-expo": "cubic-bezier(0.95, 0.05, 0.795, 0.035)",
+        "out-expo": "cubic-bezier(0.19, 1, 0.22, 1)",
+        0: "0ms",
+        2000: "2000ms",
+        height: "height",
+        spacing: "margin, padding",
+        keyframes: {
+          wiggle: {
+            "0%, 100%": { transform: "rotate(-3deg)" },
+            "50%": { transform: "rotate(3deg)" },
+          },
+          marquee: {
+            "0%": { transform: "translateX(100%)" },
+            "100%": { transform: "translateX(-200%)" },
+          },
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {
+      animation: ["hover", "focus"],
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
